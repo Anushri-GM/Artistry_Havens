47ef450d44331e12ed53c9b7223769db34539a93
Anushri <anushritcs@gmail.com>
2025-09-19 01:06:29 +0000

    adjust the splash screen to the left so that the entire image is visible

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -17,7 +17,7 @@ export function SplashScreen() {
   }, [router]);
 
   return (
-    <div className="relative flex h-screen w-screen items-center justify-center bg-background animate-in fade-in-0 duration-1000">
+    <div className="relative flex h-screen w-screen items-center justify-start bg-background animate-in fade-in-0 duration-1000">
         <Image 
             src="https://image2url.com/images/1758193888034-dbd65cc5-da9e-49fd-8766-4f0a1903d5f0.jpg"
             alt="Artisan hands working on a craft"
